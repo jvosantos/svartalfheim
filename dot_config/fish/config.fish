b7b0299b3d89714db256fbf8c653127e87310e58
Vasco Santos <jvosantos@gmail.com.com>
2020-07-10 14:23:20 +0530

    Current configuration of dot_resources

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,49 @@
+
+# set in ~/.config/fish/functions/fish_greeting.fish
+# function fish_greeting sygin_greeting
+function sygin_greeting -d "sigyin hello"
+#       .-'''-. .-./`)   .-_'''-.      ____     __ ,---.   .--.
+#      / _     \\ .-.') '_( )_   \     \   \   /  /|    \  |  |
+#     (`' )/`--'/ `-' \|(_ o _)|  '     \  _. /  ' |  ,  \ |  |
+#    (_ o _).    `-'`"`. (_,_)/___|      _( )_ .'  |  |\_ \|  |
+#     (_,_). '.  .---. |  |  .-----. ___(_ o _)'   |  _( )_   |
+#    .---.  \  : |   | '  '\ '-   .'|   |(_,_)'    | (_ o _)  |
+#    \    `-'  | |   |  \  `-'`   | |   `-'  /     |  (_,_)   |
+#     \       /  |   |   \        /  \      /      |  |    |  |
+#      `-...-'   '---'    `'-...-'    `-..-'       '--'    '--'
+    echo "                                                          "
+    echo "   .-'''-. .-./`)   .-_'''-.      ____     __ ,---.   .--."
+    echo "  / _     \\\\ .-.') '_( )_   \     \   \   /  /|    \  |  |"
+    echo " (`' )/`--'/ `-' \|(_ o _)|  '     \  _. /  ' |  ,  \ |  |"
+    echo "(_ o _).    `-'`\"`. (_,_)/___|      _( )_ .'  |  |\_ \|  |"
+    echo " (_,_). '.  .---. |  |  .-----. ___(_ o _)'   |  _( )_   |"
+    echo ".---.  \  : |   | '  '\ '-   .'|   |(_,_)'    | (_ o _)  |"
+    echo "\    `-'  | |   |  \  `-'`   | |   `-'  /     |  (_,_)   |"
+    echo " \       /  |   |   \        /  \      /      |  |    |  |"
+    echo "  `-...-'   '---'    `'-...-'    `-..-'       '--'    '--'"
+    echo "                                                          "
+    echo "=========================================================="
+end
+
+set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+set -x PATH /usr/local/opt/grep/libexec/gnubin $PATH
+
+set -x EDITOR vim
+
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+
+alias ls='gls --color=auto'
+
+# Bootstrap fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+    fish -c fisher
+end
+
+# Bootstrap Brew
+if brew bundle check --global
+    brew bundle install --global
+end
+
---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+# create soft links
+ln -s $(pwd)/dot_Brewfile ~/.Brewfile
+ln -s $(pwd)/dot_Brewfile.lock.json ~/.Brewfile.lock.json
+ln -s $(pwd)/dot_config ~/.config
+ln -s $(pwd)/dot_resources ~/.resources
+ln -s $(pwd)/dot_gitignore ~/.gitignore
+ln -s $(pwd)/dot_gitconfig ~/.gitconfig
+ln -s $(pwd)/dot_vim ~/.vim
+ln -s $(pwd)/dot_vimrc ~/.vimrc
+
+# Install xcode
+if ! [ $(xcode-select -p) ]; then
+    xcode-select --install
+fi
+
+# Install homebrew and brew bundle
+if ! [ $(which brew) ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew update
+    brew upgrade
+    brew bundle --global
+fi
+
+# install material design colors on iterm
+open "dot_resources/itermcolors/material-design-colors.itermcolors"
+# install nerd font on system
+open -W "dot_resources/nerd_font.ttf"
+
+# make fish default shell
+echo "$(command -v fish)" | sudo tee -a /etc/shells
+chsh -s "$(command -v fish)"
+
+# Post install notes
+echo "Apply color preset"
+echo " - Open iTerm -> Preferences -> Profiles -> Colors -> Presets and apply color preset"
+echo "Apply nerd font"
+echo " - Open iTerm -> Preferences -> Profiles -> Text -> Font and apply font (for non-ASCII as well)"
+
